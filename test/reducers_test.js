90792e76df545455f8e9e80f212823f7c478c3ef
toolbuilder <k_d_hudson@yahoo.com>
2019-09-21 20:41:32 -0400

    browser testing using electron

--- test/reducers_test.js
+++ test/reducers_test.js
@@ -1,19 +1,19 @@
 import { test as tape } from 'zora'
-import * as Reducers from '../src/reducers.js'
+import * as reducers from '../src/reducers.js'
 
 const testSequence = () => Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)[Symbol.iterator]()
 
 tape('forEach', test => {
   const accumulator = []
-  Reducers.forEach(x => accumulator.push(2 * x), testSequence())
+  reducers.forEach(x => accumulator.push(2 * x), testSequence())
   test.deepEqual(accumulator, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18], 'calls function for each element')
 })
 
 tape('reduce', test => {
-  const accumulator = Reducers.reduce((a, x) => { a.push(2 * x); return a }, [], testSequence())
+  const accumulator = reducers.reduce((a, x) => { a.push(2 * x); return a }, [], testSequence())
   test.deepEqual(accumulator, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18], 'calls function for each element')
 })
 
 tape('toArray', test => {
-  test.deepEqual(Reducers.toArray(testSequence()), Array.from(testSequence()), 'converts iterable values to array')
+  test.deepEqual(reducers.toArray(testSequence()), Array.from(testSequence()), 'converts iterable values to array')
 })
